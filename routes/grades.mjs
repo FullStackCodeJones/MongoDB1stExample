bc3fd47a13de3e1878890e767121eaf50b5739ce
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-12-03 09:25:22 -0500

    Notes for Limit and Skip along with CRUD

--- routes/grades.mjs
+++ routes/grades.mjs
@@ -4,6 +4,25 @@ import db from "../db/conn.mjs";
 
 const router = express.Router();
 
+// Full CRUD functionality
+//C- CREATE - Post
+//R- READ - Get
+//U -UPDATE - Put/Patch
+//D - DELETE - Delete
+
+//For Read, we usually do an index route AND  a show route
+//Index displays or gets many db items
+//Show displays one, usually based on the id
+
+//---------------------------------------------------------
+//        ALL ROUTES
+//-----------------------------------------------------
+
+//Get Implements READ Functionality
+// Be careful with the get route because it could be a huge amount of data
+//=======Make sure that you are using async and await
+//because database access request for asynchronous, but we need that data before we move on
+
 router.get("/", async (req, res) => {
   let collection = await db.collection("grades");
 
